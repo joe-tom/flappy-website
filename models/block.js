6c2100a255bdc6f948b34b9f772493b88e355dc8
joe-tom <joey6978@gmail.com>
2017-11-30 22:45:10 -0800

    Wrote waterfall for fetching last-n blocks

--- models/block.js
+++ models/block.js
@@ -1,7 +1,9 @@
 const async = require('async')
 const bitPony = require('bitpony')
 
-const flappycoin = new require('flappycoin').Cient({
+const flappycoin = require('flappycoin')
+
+var client = new flappycoin.Client({
     user: 'alice',
     pass: 'joe',
     port: 8332
@@ -9,19 +11,31 @@ const flappycoin = new require('flappycoin').Cient({
 
 
 var last = (amount) => {
+    var grab = (acc, hash, cb) => {
+        flappycoin.cmd('getblock', hash, (err, json) => {
+            acc.push(json)
+            cb(null, acc, json.previousblockhash)
+        })
+    }
+    var fall = [
+        cb => {
+            flappycoin.cmd('getbestblockhash', (err, hash) => {
+                cb(null, [], hash)
+            })
+        },
+    ]
+
+    for(var i = amount; i--;) {
+        fall.push(grab)
+    }
 
     return new Promise((resolve, reject) => {
-        async.waterfall([
-            cb => {
-                flappycoin.cmd('getbestblockhash', cb)
-            },
-            (hash, cb) => {
-                flappycoin.cmd('getblock', hash, (err, json) => {
-                    console.log(err,json)
-                })
-            }
-        ])
+        async.waterfall(fall, (err, final, lasthash) => {
+            console.log(final)
+        })
     })
 }
 
+
+last(+process.argv[3])
 
